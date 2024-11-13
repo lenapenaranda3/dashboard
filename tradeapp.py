85ca7822c6b63d8617c2cf97e9f73814df794293
lenapenaranda3 <158089465+lenapenaranda3@users.noreply.github.com>
2024-11-13 15:26:20 -0500

    Update tradeapp.py

--- tradeapp.py
+++ tradeapp.py
@@ -2,6 +2,55 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
+#######################
+# CSS styling
+st.markdown("""
+<style>
+
+[data-testid="block-container"] {
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 1rem;
+    padding-bottom: 0rem;
+    margin-bottom: -7rem;
+}
+
+[data-testid="stVerticalBlock"] {
+    padding-left: 0rem;
+    padding-right: 0rem;
+}
+
+[data-testid="stMetric"] {
+    background-color: #393939;
+    text-align: center;
+    padding: 15px 0;
+}
+
+[data-testid="stMetricLabel"] {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+[data-testid="stMetricDeltaIcon-Up"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+[data-testid="stMetricDeltaIcon-Down"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+</style>
+
+
 # Title and Description
 st.title("U.S. Trade Data Dashboard")
 st.subheader("Overview of Exports, Imports, and Trade Deficit Over Time")
